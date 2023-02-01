0910f58a085e3f91937b9de46fad5e8645008216
Hamdaan Khalid <hkhalid@microsoft.com>
2023-02-01 07:00:34 -0800

    increase system testing load

--- main.go
+++ main.go
@@ -1,59 +1,1 @@
-package main
-
-import (
-	"github.com/gorilla/mux"
-	"github.com/hamdaankhalid/consistenthashing/consistenthashing"
-	"github.com/hamdaankhalid/consistenthashing/proxy"
-	"github.com/hamdaankhalid/consistenthashing/servers"
-	"github.com/hamdaankhalid/consistenthashing/systemtesting"
-	"hash/fnv"
-	"net/http"
-	"os"
-)
-
-/*
-Usage:
-To run test demo
-
-INSTANTIATE NODE SERVERS
-go run main.go 8040 node
-go run main.go 8060 node
-go run main.go 8080 node
-
-INSTANTIATE PROXY SERVER
-go run main.go 8020 proxy
-
-SET-OFF DEMO TEST
-go run main.go test localhost:8020 localhost:8040 localhost:8060 localhost:8080
-*/
-func main() {
-	var r *mux.Router
-	if os.Args[2] == "proxy" {
-		hash := func(s string) int {
-			h := fnv.New32a()
-			_, _ = h.Write([]byte(s))
-			return int(h.Sum32())
-		}
-		hmp := consistenthashing.New(
-			"/keys",
-			"/key",
-			"/key",
-			"/key",
-			hash,
-			360,
-		)
-		r = proxy.New(hmp)
-	} else if os.Args[2] == "node" {
-		r = servers.GetApp()
-	} else if os.Args[1] == "test" {
-		masterAddress := os.Args[2]
-		nodes := os.Args[3:]
-		systemtesting.Run(100, masterAddress, nodes)
-		return
-	}
-
-	err := http.ListenAndServe(":"+os.Args[1], r)
-	if err != nil {
-		return
-	}
-}
+package mainimport (	"github.com/gorilla/mux"	"github.com/hamdaankhalid/consistenthashing/consistenthashing"	"github.com/hamdaankhalid/consistenthashing/proxy"	"github.com/hamdaankhalid/consistenthashing/servers"	"github.com/hamdaankhalid/consistenthashing/systemtesting"	"hash/fnv"	"net/http"	"os")/*Usage:To run test demoINSTANTIATE NODE SERVERSgo run main.go 8040 nodego run main.go 8060 nodego run main.go 8080 nodeINSTANTIATE PROXY SERVERgo run main.go 8020 proxySET-OFF DEMO TESTgo run main.go test localhost:8020 localhost:8040 localhost:8060 localhost:8080*/func main() {	var r *mux.Router	if os.Args[2] == "proxy" {		hash := func(s string) int {			h := fnv.New32a()			_, _ = h.Write([]byte(s))			return int(h.Sum32())		}		hmp := consistenthashing.New(			"/keys",			"/key",			"/key",			"/key",			hash,			360,		)		r = proxy.New(hmp)	} else if os.Args[2] == "node" {		r = servers.GetApp()	} else if os.Args[1] == "test" {		masterAddress := os.Args[2]		nodes := os.Args[3:]		systemtesting.Run(200, masterAddress, nodes)		return	}	err := http.ListenAndServe(":"+os.Args[1], r)	if err != nil {		return	}}
